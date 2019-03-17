f32513cc72ab9dc4cbfe255930681f8ba129b696
javiel <zehua046@gmail.com>
2019-03-18 00:31:41 +0800

    weather api

---
+++ app/controllers/cities_controller.rb
@@ -0,0 +1,14 @@
+class CitiesController < ApplicationController
+
+  def index
+      @cities = City.all
+  end
+
+  def update_temp
+    city = City.find(params[:id])
+    response = RestClient.get "http://v.juhe.cn/weather/index", :params => {:cityname => city.juhe_id, :key => JUHE_CONFIG["api_key"]}
+    data = JSON.parse(response.body)
+    city.update(:current_temp => data["result"]["sk"]["temp"])
+    redirect_to cities_path
+  end
+end
