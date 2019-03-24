93f155bdd45dca8bb9c5fa6c16aa1633a9edfa59
javiel <zehua046@gmail.com>
2019-03-24 17:12:52 +0800

    finish 3rd chapter

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  namespace :api, :defaults => {:format => :json} do
+    namespace :v1 do
+      get "/trains" => "trains#index", :as => :trains
+      get "/trains/:train_number" => "trains#show", :as => :train
+      get "/reservations/:booking_code" => "reservations#show", :as => :reservation
+      post "/reservations" => "reservations#create", :as => :create_reservations
+      patch "/reservations/:booking_code" => "reservations#update", :as => :update_reservation
+      delete "/reservations/:booking_code" => "reservations#destroy", :as => :cancel_reservation
+    end
+  end
+
   resources :cities do
     member do
       post :update_temp
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190317103800) do
+ActiveRecord::Schema.define(version: 20190323080849) do
 
   create_table "cities", force: :cascade do |t|
     t.string   "juhe_id"
@@ -21,6 +21,28 @@ ActiveRecord::Schema.define(version: 20190317103800) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.index ["juhe_id"], name: "index_cities_on_juhe_id"
+  end
+
+  create_table "reservations", force: :cascade do |t|
+    t.string   "booking_code"
+    t.integer  "train_id"
+    t.string   "seat_number"
+    t.integer  "user_id"
+    t.string   "customer_name"
+    t.string   "customer_phone"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+    t.index ["booking_code"], name: "index_reservations_on_booking_code"
+    t.index ["seat_number"], name: "index_reservations_on_seat_number"
+    t.index ["train_id"], name: "index_reservations_on_train_id"
+    t.index ["user_id"], name: "index_reservations_on_user_id"
+  end
+
+  create_table "trains", force: :cascade do |t|
+    t.string   "number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["number"], name: "index_trains_on_number"
   end
 
 end
